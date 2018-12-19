07cc56782d7e6d302867f4f383491a393fc7a7f2
Phil de Joux <philderbeast@gmail.com>
2018-12-19 13:01:59 -0500

    Download the JSON files.

---
+++ download-json.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir json
+curl -X GET -H "Accept:application/json" http://localhost:3000/comps | jq > json/comps.json
+curl -X GET -H "Accept:application/json" http://localhost:3000/nominals | jq > json/nominals.json
+curl -X GET -H "Accept:application/json" http://localhost:3000/pilots | jq > json/pilots.json
+curl -X GET -H "Accept:application/json" http://localhost:3000/tasks | jq > json/tasks.json
+
+mkdir json/gap-point
+curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/allocation | jq > json/gap-point/allocation.json
+curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/validity | jq > json/gap-point/validity.json
+
+mkdir json/task-length
+for t in {1..3}
+    do
+        mkdir json/gap-point/$t
+        curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/$t/validity-working | jq > json/gap-point/$t/validity-working.json
+        curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/$t/score | jq > json/gap-point/$t/score.json
+
+        mkdir json/task-length/$t
+        curl -X GET -H "Accept:application/json" http://localhost:3000/task-length/$t/spherical-edge | jq > json/task-length/$t/spherical-edge.json
+    done
