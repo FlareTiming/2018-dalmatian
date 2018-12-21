b6343c3e93a101de5376e45aa8cd8904d1e8fcb5
Phil de Joux <philderbeast@gmail.com>
2018-12-21 13:31:37 -0500

    Redo the downloads with cross-zone/#task/pilot-dnf.

--- download-json.sh
+++ download-json.sh
@@ -10,9 +10,13 @@ mkdir json/gap-point
 curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/allocation | jq > json/gap-point/allocation.json
 curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/validity | jq > json/gap-point/validity.json
 
+mkdir json/cross-zone
 mkdir json/task-length
 for t in {1..3}
     do
+        mkdir json/cross-zone/$t
+        curl -X GET -H "Accept:application/json" http://localhost:3000/cross-zone/$t/pilot-dnf | jq > json/cross-zone/$t/pilot-dnf.json
+
         mkdir json/gap-point/$t
         curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/$t/validity-working | jq > json/gap-point/$t/validity-working.json
         curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/$t/score | jq > json/gap-point/$t/score.json
