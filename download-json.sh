ac6e66f9f165180e5dca75df05b14db7f76b9b17
Phil de Joux <philderbeast@gmail.com>
2019-04-28 13:11:39 -0400

    Deploy app-view-0.19.

--- download-json.sh
+++ download-json.sh
@@ -18,6 +18,7 @@ curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/pilots-
 mkdir json/fs-score
 mkdir json/cross-zone
 mkdir json/cross-zone/track-flying-section
+mkdir json/tag-zone
 mkdir json/pilot-track
 mkdir json/mask-track
 mkdir json/land-out
@@ -54,13 +55,12 @@ for t in {1..3}
         curl -X GET -H "Accept:application/json" http://localhost:3000/gap-point/$t/score | jq > json/gap-point/$t/score.json
 
         mkdir json/cross-zone/track-flying-section/$t
-        for p in {4,9,13,16,21,23,26,37,40,41,46,114,54,57,63,72,73,74,80,82,86,88,91,95,98,101,102,113}
-            do
-            curl -X GET -H "Accept:application/json" http://localhost:3000/cross-zone/track-flying-section/$t/$p | jq > json/cross-zone/track-flying-section/$t/$p.json
-            done
+        mkdir json/tag-zone/$t
         mkdir json/pilot-track/$t
         for p in {4,9,13,16,21,23,26,37,40,41,46,114,54,57,63,72,73,74,80,82,86,88,91,95,98,101,102,113}
             do
+            curl -X GET -H "Accept:application/json" http://localhost:3000/cross-zone/track-flying-section/$t/$p | jq > json/cross-zone/track-flying-section/$t/$p.json
+            curl -X GET -H "Accept:application/json" http://localhost:3000/tag-zone/$t/$p | jq > json/tag-zone/$t/$p.json
             curl -X GET -H "Accept:application/json" http://localhost:3000/pilot-track/$t/$p | jq > json/pilot-track/$t/$p.json
             done
     done
